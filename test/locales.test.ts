be24453e09939b5481ff7d8de9b630d642b6f8e4
gkhanduman <c2@gkhan.me>
2023-12-02 13:29:24 +0300

    feat:test en lang

--- test/locales.test.ts
+++ test/locales.test.ts
@@ -1,7 +1,10 @@
-import { fr } from '../src';
+import { fr, en } from '../src';
 
 describe('Locales', () => {
   it('FR', () => {
     expect(fr).toBeTruthy();
+  });
+  it('EN', () => {
+    expect(en).toBeTruthy();
   });
 });
